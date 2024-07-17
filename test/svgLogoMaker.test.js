90714b79751dc30d983bc683474359eed08e76c2
Daniel D <ddunnemann@gmail.com>
2024-07-17 17:43:29 -0500

    Baby steps. Initial testing put together for validation of first prompt. Once tested satisfactorily I began adding the additional prompts according to the specifications.

---
+++ test/svgLogoMaker.test.js
@@ -0,0 +1,34 @@
+const inquirer = require('inquirer');
+
+describe('svgLogoMaker', () => {
+ describe('inquirer.prompt', () => {
+    describe('validate', () => {
+        it('should log an error message when more than 3 characters entered.', async () => {
+            // returns true when input length is less than or equal to 3 characters.
+            const input = 'ab1.';
+            const validateFunction = (input) => {
+                    if (input.length <= 3) {
+                    return true;
+                    } else { 
+                    return 'Please enter no more than 3 characters.';
+                    }
+                    };
+                  const result = validateFunction(input);
+                  expect(result).toBe('Please enter no more than 3 characters.');
+                });
+        it('should accept input when less than or equal to 3 characters entered.', async () => {
+            // returns false when more than 3 characters.
+            const input = 'abcd';
+            const validateFunction = (input) => {
+                    if (input.length <= 3) {
+                    return true;
+                    } else { 
+                    return 'Please enter no more than 3 characters.';
+                    }
+                    };
+                  const result = validateFunction(input);
+                  expect(result).toBe(true);
+                });
+            });
+     })
+    });
