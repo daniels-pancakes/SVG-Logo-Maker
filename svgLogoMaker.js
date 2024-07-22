2389e774799a602c39c4da1359f7427db8e99f4b
Daniel D <ddunnemann@gmail.com>
2024-07-22 08:33:57 -0500

    Putting together a very basic rudimentary skeleton of the app functionality. Script is taking input and pushing out to .xml file.

--- svgLogoMaker.js
+++ svgLogoMaker.js
@@ -1,37 +1,57 @@
 const inquirer = require('inquirer');
+const shape = require('./lib/shapes.js');
+const fs = require('fs');
 
-inquirer.prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter text (3 char. max):',
-      validate: function(input) {
-        if (input.length <= 3){
-        return true;} else { return 'Please enter no more than 3 characters.';};
-      } 
-    },
-    {
+let lgTxt;
+let lgTxtColor;
+let lgShp;
+let lgShpColor;
+function writeToFile(filename, data) {
+    fs.writeFile('logo.xml', data, (err) => err ? console.log(err) :
+    console.log('logo.xml file created in the current directory.'));
+}
+const init = function() {
+    inquirer.prompt([
+        {
         type: 'input',
-        name: 'textColor',
-        message: 'Enter your text color (by name or hex):',
+        name: 'text',
+        message: 'Enter text *(3 char. max):',
         validate: function(input) {
-        if (input.length = 0 || input === '') {
-        return 'Not a valid color, please try again.';};
-      } 
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Select a shape:',
-        choices: ['circle', 'triangle', 'square']
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'Enter your shape color (by name or hex):',
-        validate: function(input) {
-        if (input.length <= 3){
-        return true;} else { return 'Please enter no more than 3 characters.';};
-      } 
-    }
-]);
+            if (input.length <= 3){
+            return true;} else { return 'Please enter no more than 3 characters.';};
+            } 
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter your text color (by name or hex):',
+            validate: function(input) {
+            if (input.length === 0 || input === '') {
+            return 'Not a valid color, please try again.';} else { return true;};
+            } 
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Select a shape:',
+            choices: ['circle', 'triangle', 'square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter your shape color (by name or hex):',
+            validate: function(input) {
+            if (input.length <= 3){
+            return true;} else { return 'Please enter no more than 3 characters.';};
+            } 
+        }
+    ]).then(answers => {
+        console.log('Your answers:', answers);
+        lgTxt = answers.text;
+        lgShp = answers.shape;
+        lgTxtColor = answers.textColor;
+        lgShpColor = answers.shapeColor;
+        writeToFile('logo.xml', `${answers.text}`)
+    });
+};
+init();
